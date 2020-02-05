c36574436df559a1bc3a31e19dbf15daf295709a
Raf Sobol <rafal.sobol@academytrainee.com>
2020-02-05 14:08:12 +0000

    Updated Factorials.

--- src/com/qa/factorial/Factorials.java
+++ src/com/qa/factorial/Factorials.java
@@ -3,21 +3,27 @@ package com.qa.factorial;
 public class Factorials {
 
 	public static void main(String[] args) {
-		
+		System.out.println(factorial(479001601));
 	}
-	public static int factorial(){
+	public static int factorial(int input){
 		
-		int factorialValue = 150;
 		int value = 0;
-		
-		for(int i =1; i<10;i++) {
-			
-			value = factorialValue/i;
-			System.out.println(value);
-			
-			
+		while (input > 1) {
+			value++;
+			input = input / value;
+			System.out.println("Step "+value+", input is now "+input);
 		}
-		return value;
+		if (input == 0) return 0;
+		else return value;
+		
+//		for(int i =1; i<10;i++) {
+//			
+//			value = factorialValue/i;
+//			System.out.println(value);
+//			
+//			
+//		}
+//		return value;
 	}
 		
 	
